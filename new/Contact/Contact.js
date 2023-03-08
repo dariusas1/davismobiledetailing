e998a1f72401071d76d1f78cfeaa09aaf117e83d
ljv29 <jumpswervelogan@gmail.com>
2023-03-07 23:40:16 -0700

    removed old fodler

--- new/Contact/Contact.js
+++
@@ -1,7 +0,0 @@
-const bars = document.querySelector("#mobile-bars");
-const dropdown = document.querySelector(".navbar-dropdown");
-bars.addEventListener("click", () => {
-    dropdown.classList.toggle("active");
-    bars.classList.toggle("active");
-    navbar.classList.toggle("active");
-});
